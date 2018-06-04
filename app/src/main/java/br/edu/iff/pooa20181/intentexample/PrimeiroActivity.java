ad4cc41975f7c049d212a7e4da360f8b5bb523b0
Lucas <lucasmisidoro@gmail.com>
2018-06-04 19:34:06 -0300

    Abrindo o navegador com a Intent

--- app/src/main/java/br/edu/iff/pooa20181/intentexample/PrimeiroActivity.java
+++ app/src/main/java/br/edu/iff/pooa20181/intentexample/PrimeiroActivity.java
@@ -1,7 +1,15 @@
 package br.edu.iff.pooa20181.intentexample;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.net.URI;
+import java.net.URL;
 
 public class PrimeiroActivity extends AppCompatActivity {
 
@@ -9,5 +17,19 @@ public class PrimeiroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final EditText edtURI = (EditText) findViewById(R.id.edtURI);
+        Button btOk = (Button) findViewById(R.id.btnOK);
+
+        btOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse(edtURI.getText().toString());
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+
+            }
+        });
     }
 }
